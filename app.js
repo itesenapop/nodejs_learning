18322ba53923c34b23f41a328361110936e1f857
itesenapop <itsena.pop@gmail.com>
2023-03-06 21:05:21 +0700

    full1

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+
+app.use(morgan('combined'));
+
+app.get("/",(req,res) =>{
+res.send('Hello John.');
+
+})
+
+app.listen(port,()=>{
+console.log("Listening on port "+ chalk.blue(port));
+
+})
